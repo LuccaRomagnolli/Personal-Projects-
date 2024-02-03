7c3d9813a9ba706290eb6da71ee7f1f5643c7aee
Lucca Romagnolli <lucca2408@gmail.com>
2024-02-02 22:28:41 -0300

    Create Pandas.py

---
+++ Pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+person = {
+    'name': ['John','Annie','Mark','Josh'],
+    'last': ['Macfly','Storm','Breadley','Senna'],
+    'adress': ['John31_@gmail.com','AnnieSt@hotmail.com','Mr992@outlook.com','JoshFin@gmail.com']
+}
+df = pd.DataFrame(person)
+print(df)
